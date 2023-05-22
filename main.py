3070a0f8c47c4f2e7a5c3e2b1155148eca7cc524
2413567 <51052656+2413567@users.noreply.github.com>
2023-05-22 21:18:27 +0800

    对代码封装

--- main.py
+++ main.py
@@ -1,43 +1,59 @@
-import os.path
+import os
 import pandas as pd
 import sqlalchemy
 
-# 连接MySQL数据库
-connection = sqlalchemy.create_engine('mysql+pymysql://root:root@host/databases?charset=utf8').connect()
+# 连接到数据库
+def connect_to_database(connection_str):
+    return sqlalchemy.create_engine(connection_strconnect()
 
-# 获取数据库中的所有表名
-inspector = sqlalchemy.inspect(connection)
-tables = inspector.get_table_names()
+# 获取数据库中的表和列信息
+def get_tables_and_columns(connection):
+    inspector = sqlalchemy.inspect(connection)
+    tables = inspector.get_table_names()
+    table_info = {}
+    for table in tables:
+        columns = inspector.get_columns(table)
+        table_comment = inspector.get_table_comment(table)
+        table_info[table] = {
+            'columns': columns,
+            'comment': table_comment['text'] if table_comment['text'] else table
+        }
+    return table_info
 
-# 创建保存数据的目录（如果不存在）
-os.makedirs('数据', exist_ok=True)
-os.makedirs('数据1', exist_ok=True)
+# 将数据保存到文件
+def save_data_to_files(connection, table_info):
+    # 创建输出目录
+    os.makedirs('数据', exist_ok=True)
+    os.makedirs('数据1', exist_ok=True)
 
-# 遍历每个表名，读取数据并保存为pkl文件
-for table in tables:
-    # 获取表结构信息
-    columns = inspector.get_columns(table)
-    table_comment = inspector.get_table_comment(table)
+    for table, info in table_info.items():
+        columns = info['columns']
+        # 获取列注释
+        column_comments = [column['comment'] if column['comment'] else column['name'] for column in columns]
 
-    # 解析字段名和注释
-    column_names = [column['name'] for column in columns]
-    column_comments = [column['comment'] if column['comment'] else column['name'] for column in columns]
+        # 查询表数据
+        query = f"SELECT * FROM {table}"
+        data = pd.read_sql(sql=query, con=connection)
+        # 重命名列名为注释
+        data.columns = column_comments
 
-    # 读取表数据
-    query = f"SELECT * FROM {table}"
-    query = sqlalchemy.text(query)
-    data = pd.read_sql(sql=query, con=connection)
+        # 保存数据到文件
+        filename = info['comment']
+        data.to_pickle(os.path.join('数据', f"{filename}.pkl"))
+        data.head().astype('str').to_excel(os.path.join('数据1', f"{filename}.xlsx"), index=False)
+        print(filename)
 
-    # 为DataFrame设置字段名和注释
-    data.columns = column_comments
+def main():
+    # 数据库连接字符串
+    db_connection_str = 'mysql+pymysql://root:root@host/databases?charset=utf8'
+    # 连接到数据库
+    connection = connect_to_database(db_connection_str)
+    # 获取表和列信息
+    table_info = get_tables_and_columns(connection)
+    # 保存数据到文件
+    save_data_to_files(connection, table_info)
+    # 关闭数据库连接
+    connection.close()
 
-    # 保存为pkl文件，以注释作为文件名
-    filename = table_comment['text'] if table_comment['text'] else table
-    data.to_pickle(os.path.join('数据', f"{filename}.pkl"))
-
-    # 保存为Excel文件，以注释作为文件名
-    data.head().astype('str').to_excel(os.path.join('数据1', f"{filename}.xlsx"), index=False)
-    print(filename)
-
-# 关闭数据库连接
-connection.close()
+if __name__ == '__main__':
+    main()
